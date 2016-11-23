a66246cd383719451b81f2654cd65b34ab370ed0
Gerardo Teruel <gerardogtn@gmail.com>
2016-11-23 17:57:11 -0600

    :rocket: Add SchedulerResult

---
+++ SchedulerResult.hpp
@@ -0,0 +1,31 @@
+// Copyright 2016
+#ifndef SCHEDULERRESULT_H
+#define SCHEDULERRESULT_H
+
+class SchedulerResult {
+ private:
+  double averageWaitTime;
+  double averageTurnAroundTime;
+
+ public:
+  SchedulerResult() {}
+  virtual ~SchedulerResult() {}
+
+  void setAverageWaitTime(double averageWaitTime) {
+    this->averageWaitTime = averageWaitTime;
+  }
+
+  void setAverageTurnAroundTime(double averageTurnAroundTime) {
+    this->averageTurnAroundTime = averageTurnAroundTime;
+  }
+
+  double getAverageWaitTime() {
+    return averageWaitTime;
+  }
+
+  double getAverageTurnAroundTime() {
+    return averageTurnAroundTime;
+  }
+};
+
+#endif  // SCHEDULERRESULT_H
