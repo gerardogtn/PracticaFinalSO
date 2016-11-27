c69a223379d8759096e79984f374dff277ba26eb
Gerardo Teruel <gerardogtn@gmail.com>
2016-11-27 15:35:55 -0600

    :bug: Add include Process

--- processComparison.hpp
+++ processComparison.hpp
@@ -2,6 +2,8 @@
 #ifndef PROCESSCOMPARISON_H
 #define PROCESSCOMPARISON_H
 
+#include "Process.hpp"
+
 bool compareByArrivalTime(const Process& first, const Process& second) {
   return first.getArrivalTime() < second.getArrivalTime();
 }
